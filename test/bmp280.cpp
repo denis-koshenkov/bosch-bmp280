34aee9c2d162b52d0b04a1a010cedef054693221
Denis Koshenkov <dkoshenkov@gmail.com>
2026-02-05 07:54:41 +0100

    set_temp_oversampling cannot be interrupted

--- test/bmp280.cpp
+++ test/bmp280.cpp
@@ -1858,3 +1858,109 @@ TEST(BMP280, InitMeasCannotBeInterruptedReadSuccess)
     uint8_t read_1_rc = BMP280_IO_RESULT_CODE_OK;
     test_read_seq_cannot_be_interrupted(read_1_start_reg, read_1_num_regs, read_1_data, read_1_rc, init_meas);
 }
+
+typedef struct {
+    uint8_t reg_addr;
+    uint8_t read_1_data;
+    uint8_t read_1_rc;
+    uint8_t write_2_data;
+    uint8_t write_2_rc;
+    BMP280Function start_seq;
+} ReadWriteSeqCannotBeInterruptedTestCfg;
+
+static void test_read_write_seq_cannot_be_interrupted(const ReadWriteSeqCannotBeInterruptedTestCfg *const cfg)
+{
+    if (!cfg->start_seq) {
+        FAIL_TEST("Invalid args");
+    }
+    uint8_t rc_create = bmp280_create(&bmp280, &init_cfg);
+    CHECK_EQUAL(BMP280_RESULT_CODE_OK, rc_create);
+
+    mock()
+        .expectOneCall("mock_bmp280_read_regs")
+        .withParameter("start_addr", cfg->reg_addr)
+        .withParameter("num_regs", 1)
+        .withOutputParameterReturning("data", &cfg->read_1_data, 1)
+        .withParameter("user_data", read_regs_user_data)
+        .ignoreOtherParameters();
+    if (cfg->read_1_rc == BMP280_IO_RESULT_CODE_OK) {
+        mock()
+            .expectOneCall("mock_bmp280_write_reg")
+            .withParameter("addr", cfg->reg_addr)
+            .withParameter("reg_val", cfg->write_2_data)
+            .withParameter("user_data", write_reg_user_data)
+            .ignoreOtherParameters();
+    }
+    mock().expectOneCall("mock_bmp280_complete_cb").ignoreOtherParameters();
+
+    uint8_t rc = cfg->start_seq();
+    CHECK_EQUAL(BMP280_RESULT_CODE_OK, rc);
+
+    uint8_t other_cmd_rc;
+    other_cmd_rc = bmp280_set_temp_oversampling(bmp280, BMP280_OVERSAMPLING_1, NULL, NULL);
+    CHECK_EQUAL(BMP280_RESULT_CODE_BUSY, other_cmd_rc);
+
+    read_regs_complete_cb(cfg->read_1_rc, read_regs_complete_cb_user_data);
+    if (cfg->read_1_rc == BMP280_IO_RESULT_CODE_OK) {
+        /* Check that sequence cannot be interrupted between read and write */
+        other_cmd_rc = bmp280_set_temp_oversampling(bmp280, BMP280_OVERSAMPLING_1, NULL, NULL);
+        CHECK_EQUAL(BMP280_RESULT_CODE_BUSY, other_cmd_rc);
+
+        write_reg_complete_cb(cfg->write_2_rc, write_reg_complete_cb_user_data);
+    }
+
+    /* Sequence finished, other operations are now allowed */
+    /* Exact value does not matter */
+    uint8_t read_2_data = 0x46;
+    mock()
+        .expectOneCall("mock_bmp280_read_regs")
+        .withParameter("start_addr", 0xF4)
+        .withParameter("num_regs", 1)
+        .withOutputParameterReturning("data", &read_2_data, 1)
+        .withParameter("user_data", read_regs_user_data)
+        .ignoreOtherParameters();
+    other_cmd_rc = bmp280_set_temp_oversampling(bmp280, BMP280_OVERSAMPLING_1, NULL, NULL);
+    CHECK_EQUAL(BMP280_RESULT_CODE_OK, other_cmd_rc);
+}
+
+TEST(BMP280, SetTempOversamplingCannotBeInterruptedReadFail)
+{
+    ReadWriteSeqCannotBeInterruptedTestCfg cfg = {
+        .reg_addr = 0xF4,
+        .read_1_data = 0xFF,
+        .read_1_rc = BMP280_IO_RESULT_CODE_ERR,
+        /* Set 3 MSb to 001 (oversampling x1), keep other bits the same */
+        .write_2_data = 0x3F,
+        .write_2_rc = BMP280_IO_RESULT_CODE_ERR,
+        .start_seq = set_temp_oversampling,
+    };
+    test_read_write_seq_cannot_be_interrupted(&cfg);
+}
+
+TEST(BMP280, SetTempOversamplingCannotBeInterruptedWriteFail)
+{
+    ReadWriteSeqCannotBeInterruptedTestCfg cfg = {
+        .reg_addr = 0xF4,
+        .read_1_data = 0xFF,
+        .read_1_rc = BMP280_IO_RESULT_CODE_OK,
+        /* Set 3 MSb to 001 (oversampling x1), keep other bits the same */
+        .write_2_data = 0x3F,
+        .write_2_rc = BMP280_IO_RESULT_CODE_ERR,
+        .start_seq = set_temp_oversampling,
+    };
+    test_read_write_seq_cannot_be_interrupted(&cfg);
+}
+
+TEST(BMP280, SetTempOversamplingCannotBeInterruptedSuccess)
+{
+    ReadWriteSeqCannotBeInterruptedTestCfg cfg = {
+        .reg_addr = 0xF4,
+        .read_1_data = 0xFF,
+        .read_1_rc = BMP280_IO_RESULT_CODE_OK,
+        /* Set 3 MSb to 001 (oversampling x1), keep other bits the same */
+        .write_2_data = 0x3F,
+        .write_2_rc = BMP280_IO_RESULT_CODE_OK,
+        .start_seq = set_temp_oversampling,
+    };
+    test_read_write_seq_cannot_be_interrupted(&cfg);
+}
